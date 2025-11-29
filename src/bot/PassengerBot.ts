d745f9f9430621f755fc7084f8448368b61592c2
ShohjahonIbrohimov <63698156+ShohjahonIbrohimov@users.noreply.github.com>
2025-11-29 14:56:20 +0500

    fix: set webhook once

--- src/bot/PassengerBot.ts
+++ src/bot/PassengerBot.ts
@@ -11,19 +11,17 @@ console.log(config.env);
 
 (async () => {
     if (config.env === "production") {
-        await userBot.setWebHook(`${config.webhookDomain}/bot${config.token}`);
-
-        // try {
-        //     const webhookInfo = await userBot.getWebHookInfo();
-        //     if (!webhookInfo.url || webhookInfo.url === "") {
-        //         await userBot.setWebHook(`${config.webhookDomain}/bot${config.token}`);
-        //         console.log("Webhook set for production");
-        //     } else {
-        //         console.log("Webhook already set, skipping setWebHook");
-        //     }
-        // } catch (err) {
-        //     console.error("Error checking/setting webhook:", err);
-        // }
+        try {
+            const webhookInfo = await userBot.getWebHookInfo();
+            if (!webhookInfo.url || webhookInfo.url === "") {
+                await userBot.setWebHook(`${config.webhookDomain}/bot${config.token}`);
+                console.log("Webhook set for production");
+            } else {
+                console.log("Webhook already set, skipping setWebHook");
+            }
+        } catch (err) {
+            console.error("Error checking/setting webhook:", err);
+        }
     }
 })();
 
