371437d5e47dff68f5c78bf3197827ef4da901fa
ShohjahonIbrohimov <63698156+ShohjahonIbrohimov@users.noreply.github.com>
2025-12-09 18:03:56 +0500

    sd

--- src/controllers/paynet.controller.ts
+++ src/controllers/paynet.controller.ts
@@ -55,9 +55,9 @@ export class PaynetCallbackController {
             }
 
             // Service enabled check
-            // if (!this.isServiceEnabled(params.serviceId)) {
-            //     throw new ServiceTemporarilyUnavailable(rpcId);
-            // }
+            if (!this.isServiceEnabled(params.serviceId)) {
+                throw new ServiceTemporarilyUnavailable(rpcId);
+            }
 
             // Route the method
             return await this.routeMethod(method, params, rpcId, res);
--- src/utils/paynet_status_code.ts
+++ src/utils/paynet_status_code.ts
@@ -1,15 +1,15 @@
 import { TransactionStatus } from "../models/transaction.model";
 
 export const StatusCodes = {
-    CREATED: 0,
-    CANCELLED: 1,
+    CREATED: "0",
+    CANCELLED: "1",
 } as const;
 
-export function statusToCode(status: TransactionStatus): number {
+export function statusToCode(status: TransactionStatus): string {
     return StatusCodes[status];
 }
 
-export function codeToStatus(code: number): TransactionStatus | null {
-    const entry = Object.entries(StatusCodes).find(([_, v]) => v === code);
-    return entry ? (entry[0] as TransactionStatus) : null;
-}
+// export function codeToStatus(code: number): TransactionStatus | null {
+//     const entry = Object.entries(StatusCodes).find(([_, v]) => v === code);
+//     return entry ? (entry[0] as TransactionStatus) : null;
+// }
