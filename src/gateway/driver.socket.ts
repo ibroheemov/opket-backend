30dcf2ebbb004558ee6128e4adc3e58969158a64
ShohjahonIbrohimov <63698156+ShohjahonIbrohimov@users.noreply.github.com>
2026-02-02 06:13:26 +0500

    Sds

--- src/gateway/driver.socket.ts
+++ src/gateway/driver.socket.ts
@@ -54,7 +54,7 @@ export const registerDriverHandlers = async ({ socket, driverId, fcmToken, locat
 
     asyncemitMissedEvents(socket, driverId);
 
-    emitToDriver(driverId, "feature_flags", { 'driverStatusToggleEnabled': false });
+    emitToDriver(driverId, "feature_flags", { 'driverStatusToggleEnabled': true });
 
     // 2️⃣ Handle location updates
     socket.on("driver_location", async ({ lat, lon, bearing }) => {
