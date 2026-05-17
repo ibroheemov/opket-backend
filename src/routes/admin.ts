bf02597fbfe0407036f228d745f9ce0ddc332a93
ShohjahonIbrohimov <63698156+ShohjahonIbrohimov@users.noreply.github.com>
2026-05-17 20:00:27 +0500

    Sd

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,13 +1,9 @@
 import express from "express";
-import { createWorkingArea, deleteWorkingArea, updateWorkingArea } from "../controllers/working_area.controller";
 import { enableServices } from "../controllers/admin.controller";
 import { emitToUser } from "../gateway/ride.socket";
 
 const router = express.Router();
 
-router.post("/working-areas", createWorkingArea);
-router.put("/working-areas/:id", updateWorkingArea);
-router.delete("/working-areas/:id", deleteWorkingArea);
 router.post("/drivers/enable-services", enableServices);
 
 router.post("/passengers/:phone/force-logout", async (req, res) => {
